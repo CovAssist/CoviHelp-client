0dd9261022acc850615d7e86c6143cb3b7e702db
Poorvi Vaish <img_2019047@iiitm.ac.in>
2021-05-17 01:09:13 +0530

    Added Donor Registration Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Disclaimer from './components/Disclaimer/Disclaimer';
-
 import LandingPage from './pages/LandingPage/LandingPage';
+import DonorRegistration from './pages/DonorRegistration/DonorRegistration';
 
 const App = () => {
   const [modal, setmodal] = useState(false);
-
+  
   const openModal = () => {
     setmodal((prev) => !prev);
   };
@@ -22,6 +22,7 @@ const App = () => {
           </Route>
           <Route path="/">
             <LandingPage />
+            <DonorRegistration />
           </Route>
         </Switch>
       </Router>
