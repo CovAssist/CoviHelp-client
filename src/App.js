67bbb548f1ddf9d946a5255e20b7a2dd8680f7f9
manikmmalhotra <manikmalhotra2113@gmail.com>
2021-05-16 14:17:38 +0530

    disclaimer functionality done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Disclaimer from './components/Disclaimer/Disclaimer';
+import { useState } from 'react';
 
 const App = () => {
+  const [modal, setmodal] = useState(false);
+
+  const openModal = () => {
+    setmodal((prev) => !prev);
+  };
   return (
     <div className="app">
-      <Disclaimer />
+      <Disclaimer modal={modal} setmodal={setmodal} />
+      <button onClick={openModal}>showModal</button>
     </div>
   );
 };
--- src/components/Disclaimer/Disclaimer.js
+++ src/components/Disclaimer/Disclaimer.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import './Disclaimer.css';
 
-function Disclaimer() {
+function Disclaimer({ modal, setmodal }) {
   return (
-    <div className="modal">
-      <div className="disclaimerBox">
-        <div className="container">
-          <div className="disclaimer">DISCLAIMER</div>
-          <div className="para">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta pulvinar massa, eu
-            lacinia lectus. Quisque eget purus quis est auctor fringilla. In dignissim consectetur
-            ipsum ac placerat. Quisque viverra purus pellentesque ultricies pretium. Nullam tortor
-            enim, volutpat et enim a, faucibus auctor risus. Morbi eu molestie massa. Integer vel
-            dui in quam pulvinar viverra a id lectus. Maecenas interdum sapien ipsum, in
-            pellentesque velit blandit gravida.
+    <>
+      {modal ? (
+        <div className="modal">
+          <div className="disclaimerBox">
+            <div className="container">
+              <div className="disclaimer">DISCLAIMER</div>
+              <div className="para">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta pulvinar massa,
+                eu lacinia lectus. Quisque eget purus quis est auctor fringilla. In dignissim
+                consectetur ipsum ac placerat. Quisque viverra purus pellentesque ultricies pretium.
+                Nullam tortor enim, volutpat et enim a, faucibus auctor risus. Morbi eu molestie
+                massa. Integer vel dui in quam pulvinar viverra a id lectus. Maecenas interdum
+                sapien ipsum, in pellentesque velit blandit gravida.
+              </div>
+              <button className="btn" onClick={() => setmodal((prev) => !prev)}>
+                {' '}
+                Accept{' '}
+              </button>
+            </div>
+            <img
+              className="image__disclaimer"
+              src="https://raw.githubusercontent.com/CovAssist/CoviHelp-client/5f68c20582f84ff988b7ad18ec0639445d441392/src/Assests/dis1.svg?token=APAWVJ7FYKLPLPPH7AH3MRLAUDODC"
+              alt=""
+            />
           </div>
-          <button className="btn"> Accept </button>
         </div>
-        <img
-          className="image__disclaimer"
-          src="https://raw.githubusercontent.com/CovAssist/CoviHelp-client/89044c7345871e3fe61313c9b74270ed4f8c6580/src/Assests/dis1.svg?token=APAWVJ7BHTXYVJ3BP3E3DLDAUDJJE"
-          alt=""
-        />
-      </div>
-    </div>
+      ) : null}
+    </>
   );
 }
 
