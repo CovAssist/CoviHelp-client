709c68de67eb33d0111153ffbc964a3153577ebd
manikmmalhotra <manikmalhotra2113@gmail.com>
2021-05-17 14:02:41 +0530

    list designed

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import './App.css';
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Disclaimer from './components/Disclaimer/Disclaimer';
-import { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Disclaimer from './components/Disclaimer/Disclaimer';
-
 import VaccantBed from './pages/VaccantBed/VaccantBed';
-
+import DonorRegistration from './pages/DonorRegistration/DonorRegistration';
+import MainHeader from './components/MainHeader/MainHeader';
+import LandingPage from './pages/LandingPage/LandingPage';
+import List from './components/List/List';
 
 const App = () => {
   const [modal, setmodal] = useState(false);
-  
+
   const openModal = () => {
     setmodal((prev) => !prev);
   };
   return (
-
     <div className="app">
       <Router>
         <Switch>
@@ -27,14 +25,18 @@ const App = () => {
           <Route path="/bed">
             <VaccantBed />
           </Route>
+          <Route path="/donor">
+            <DonorRegistration />
+          </Route>
+          <Route path="/list">
+            <List />
+          </Route>
           <Route path="/">
             <LandingPage />
-            <DonorRegistration />
           </Route>
         </Switch>
       </Router>
     </div>
-   
   );
 };
 
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './List.css';
 
+function SimpList() {
+  return (
+    <div className="simple__list">
+      <div className="simpplist">
+        <div className="simpplist__title">this is A Hospital</div>
+        <div className="simpplist__time">21 May 2021, Time</div>
+      </div>
+    </div>
+  );
+}
+
+function ClickedList() {
+  return (
+    <div className="clicked__list">
+      <div className="clicked__top">
+        <div className="clicked__elem">
+          <div className="clicked__heading">Hospital Name</div>
+          <div className="clicked__bold clicked__filing">This That Hospital</div>
+        </div>
+        <div className="clicked__elem">
+          <div className="clicked__heading">Last Time Verified</div>
+          <div className="clicked__filing">1 May 2021, 5:00PM</div>
+        </div>
+      </div>
+      <div className="clicked__bot">
+        <div className="clicked__elem">
+          <div className="clicked__heading">Address</div>
+          <div className="clicked__filing">Content here, content here</div>
+        </div>
+        <div className="clicked__elem">
+          <div className="clicked__heading">Phone no.</div>
+          <div className="clicked__filing">+91 0000000000</div>
+        </div>
+        <div className="clicked__elem">
+          <div className="clicked__heading">Details</div>
+          <div className="clicked__filing">Content here, content here</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function List() {
-  return <div></div>;
+  const [CLicked, setCLicked] = useState(true);
+  return <div className="list">{CLicked ? <ClickedList /> : <SimpList />}</div>;
 }
 
 export default List;
