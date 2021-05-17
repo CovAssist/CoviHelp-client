239b8c0c740d15812acd5975f96bceb71fd9976e
Poorvi Vaish <img_2019047@iiitm.ac.in>
2021-05-18 00:26:16 +0530

    Fixed search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,22 +3,22 @@ import './SearchBar.css';
 import Button from '../Button/Button';
 import SearchIcon from '@material-ui/icons/Search';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
+import { Link } from 'react-router-dom';
 
 const SearchBar = () => {
-
   return (
     <div className="searchBar">
       <div className="type">
         <div className="searchIcon">
           <SearchIcon />
         </div>
-        <select className="select" type="text" placeholder="select category">
-          <option value="0" hidden selected>
-            select category
-          </option>
-          <option value="1">Vacant Beds</option>
-          <option value="2">Oxygen Availability</option>
-        </select>
+        <div class="dropdown">
+          <button class="dropbtn">select category</button>
+          <div class="dropdown-content">
+            <Link to="/bed">Vacant Bed</Link>
+            <Link to="/Oxygen">Oxygen Availability</Link>
+          </div>
+        </div>
       </div>
       <div className="location">
         <div className="locateIcon">
